4364d34eccb50d2b1c400128d0499c9a3197f938
liangYiHui <156614618@qq.com>
2023-02-20 14:11:09 +0800

    end

---
+++ src/test/java/SimpleBeanContainerTest.java
@@ -0,0 +1,21 @@
+import org.junit.Test;
+import org.springframework.BeanFactory;
+
+/**
+ * ● @author: YiHui
+ * ● @date: Created in 17:58  2023/2/17
+ */
+public class SimpleBeanContainerTest {
+    @Test
+    public void test() {
+        BeanFactory beanFactory = new BeanFactory();
+        beanFactory.registerBean("userService", new UserService());
+        UserService userService = (UserService)beanFactory.getBean("userService") ;
+        userService.sayHello();
+    }
+    class UserService{
+        public void sayHello(){
+            System.out.println("helloWord");
+        }
+    }
+}
