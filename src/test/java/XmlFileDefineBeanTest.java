4fecd520c9c25e662a34aed3dd5955425e23db32
liangYiHui <156614618@qq.com>
2023-04-09 19:15:18 +0800

    fix: 测试代码和测试xml

---
+++ src/test/java/XmlFileDefineBeanTest.java
@@ -0,0 +1,20 @@
+import bean.Person;
+import org.junit.Test;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
+
+/**
+ * ● @author: YiHui
+ * ● @date: Created in 18:36  2023/4/8
+ */
+public class XmlFileDefineBeanTest {
+    @Test
+    public void testXmlFileDefine(){
+        DefaultListableBeanFactory factory = new DefaultListableBeanFactory();
+        XmlBeanDefinitionReader xmlBeanDefinitionReader = new XmlBeanDefinitionReader(factory);
+        xmlBeanDefinitionReader.loadBeanDefinitions("classpath:spring.xml");
+
+        Person person = (Person) factory.getBean("person");
+        System.out.println(person);
+    }
+}
