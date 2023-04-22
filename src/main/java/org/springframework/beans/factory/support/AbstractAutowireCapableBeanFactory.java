5ceebc037c5d248acb7d1123c4f53353352b2301
liangYiHui <156614618@qq.com>
2023-04-22 17:33:59 +0800

    refactor: 修改bean创建逻辑，添加bean初始化方法

--- src/main/java/org/springframework/beans/factory/support/AbstractAutowireCapableBeanFactory.java
+++ src/main/java/org/springframework/beans/factory/support/AbstractAutowireCapableBeanFactory.java
@@ -3,7 +3,9 @@ package org.springframework.beans.factory.support;
 import cn.hutool.core.bean.BeanUtil;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.PropertyValue;
+import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
 import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.beans.factory.config.BeanReference;
 
 import java.lang.reflect.Constructor;
@@ -24,6 +26,8 @@ public abstract class AbstractAutowireCapableBeanFactory extends AbstractBeanFac
             bean = createBeanInstance(name,beanDefinition,args);
             //填充属性
             applyPropertyValues(name,bean, beanDefinition);
+            //执行bean的初始化方法和BeanPostProcessor的前置和后置处理方法
+            initializeBean(name, bean, beanDefinition);
         }catch(Exception e){
             throw new BeansException("Fail to instantiation",e);
         }
@@ -31,7 +35,16 @@ public abstract class AbstractAutowireCapableBeanFactory extends AbstractBeanFac
         return bean;
     }
 
+    protected Object initializeBean(String beanName, Object bean,BeanDefinition beanDefinition){
+        //执行BeanPostProcessor的前置处理
+        Object wrappedBean = applyBeanPostProcessorsBeforeInitialization(bean, beanName);
 
+        // todo 执行bean初始化
+        invokeInitMethods(beanName,bean, beanDefinition);
+        //执行BeanPostProcessor的后置处理
+        wrappedBean = applyBeanPostProcessorsAfterInitialization(bean, beanName);
+        return wrappedBean;
+    }
     protected Object createBeanInstance(String name,BeanDefinition beanDefinition,Object[] args) throws Exception {
         Class clazz = beanDefinition.getClazz();
         Constructor constructor = null;
@@ -57,4 +70,41 @@ public abstract class AbstractAutowireCapableBeanFactory extends AbstractBeanFac
         }
     }
 
+    @Override public Object applyBeanPostProcessorsBeforeInitialization(Object existingBean, String beanName)
+        throws BeansException {
+        Object result = existingBean;
+        for(BeanPostProcessor beanPostProcessor : getBeanPostProcessors()){
+            Object current = beanPostProcessor.postProcessBeforeInitialization(existingBean,beanName);
+            if(current == null){
+                return result;
+            }
+            result = current;
+        }
+        return result;
+    }
+
+    @Override public Object applyBeanPostProcessorsAfterInitialization(Object existingBean, String beanName)
+        throws BeansException {
+        Object result = existingBean;
+        for(BeanPostProcessor beanPostProcessor : getBeanPostProcessors()){
+            Object current = beanPostProcessor.postProcessAfterInitialization(existingBean,beanName);
+            if(current ==null){
+                return result;
+            }
+            result = current;
+        }
+        return result;
+    }
+    /**
+     * 执行bean的初始化方法
+     *
+     * @param beanName
+     * @param bean
+     * @param beanDefinition
+     * @throws Throwable
+     */
+    protected void invokeInitMethods(String beanName, Object bean, BeanDefinition beanDefinition) {
+        //TODO 后面会实现
+        System.out.println("执行bean[" + beanName + "]的初始化方法");
+    }
 }
